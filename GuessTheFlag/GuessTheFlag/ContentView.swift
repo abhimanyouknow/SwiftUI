7407cbb0bd99ccadbe5ee443fc9078baa95f79e1
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2024-11-06 13:33:33 +0530

    Project 2 | GuessTheFlag | Part I | Initial Setup

---
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by C3PO MBP on 06/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var showingAlert = false
+    
+    var body: some View {
+        VStack {
+            Text("Project 2")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by C3PO MBP on 06/11/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

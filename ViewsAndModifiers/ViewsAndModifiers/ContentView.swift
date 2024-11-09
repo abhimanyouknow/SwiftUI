e2b7f318ee591bfe5140a728822311a45a4c2b7e
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2024-11-09 23:36:42 +0530

    Project 3 | ViewsAndModifiers | Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  ViewsAndModifiers
+//
+//  Created by C3PO MBP on 09/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by C3PO MBP on 09/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

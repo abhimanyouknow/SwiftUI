16e70f23caa39517a6eb385c6b030df53b47ba81
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2024-11-10 12:25:32 +0530

    Project 3 | Challenge 3

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -7,9 +7,36 @@
 
 import SwiftUI
 
+// custom modifier
+struct ProminentTitle: ViewModifier {
+    func body(content: Content) -> some View {
+        ZStack {
+            content
+                .font(.largeTitle)
+                .foregroundStyle(.blue)
+        }
+    }
+}
+
+// custom extension
+extension View {
+    func titleStyle() -> some View {
+        modifier(ProminentTitle())
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            RadialGradient(colors: [.blue, .black], center: .top, startRadius: 20, endRadius: 200)
+            
+            Text("Prominent Title")
+                .titleStyle()
+                /* NOTE: invoking the custom View
+                 extension which in turn invokes the
+                 custom modifier */
+        }
+        .ignoresSafeArea()
     }
 }
 

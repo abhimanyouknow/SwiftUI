878953ffbee45c1fc0c69a7bd7f913cce361ec72
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2024-11-19 19:02:06 +0530

    Project 6 | Animations | Dragging Card View

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,20 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount = 0.0
+    // step 1: we need a state to store the amout of drag
+    @State private var dragAmount = CGSize.zero
+    //@State private var dragAmount = Angle.zero
     
     var body: some View {
-        Button("Tap Me") {
-            withAnimation(.spring(duration: 1, bounce: 0.5)) {
-                animationAmount += 360
-            }
-        }
-        .padding(40)
-        .background(.red)
-        .foregroundStyle(.white)
-        .clipShape(.circle)
-        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
-            // rotating the button on the y-axis
+        LinearGradient(colors: [.yellow, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
+            .frame(width: 300, height: 200)
+            .clipShape(.rect(cornerRadius: 10))
+            .offset(dragAmount)
+                /* step 2: we need to use dragAmount to
+                 influence the position of the card on the
+                 screen -> for this we use the .offset()
+                 modifier */
+            .gesture(
+                DragGesture()
+                    .onChanged { dragAmount = $0.translation }
+                    .onEnded { _ in
+                        withAnimation {
+                            dragAmount = .zero
+                        }
+                    }
+            )
+                /* step 3: create a drag gesture object and
+                 attach it to the card */
+            
+        
+//            .gesture(
+//                RotateGesture()
+//                    .onChanged { dragAmount = $0.rotation }
+//                    .onEnded { _ in dragAmount = .zero }
+//            )
     }
 }
 

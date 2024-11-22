a92afcfd375dadc474ec603f3768f3a00e399d2d
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2024-11-22 18:49:57 +0530

    Project 7 | iExpense | Showing & Hiding Views using .sheet

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -7,23 +7,40 @@
 
 import SwiftUI
 
-@Observable
-class User {
-    var firstName = "Wayne"
-    var lastName = "Rooney"
+// this is the second view that we want to show
+struct SecondView: View {
+    @Environment(\.dismiss) var dismiss
+        /* environment property to manually dismiss the second
+         view */
+    let name: String
+    
+    var body: some View {
+        Text("Hello, \(name)!")
+        Button("Dismiss") {
+            dismiss() // calling the dismiss method
+        }
+    }
 }
 
 struct ContentView: View {
-    @State private var user = User()
+    /* step 1: adding a state property to track whether the
+     sheet is showing or not */
+    @State private var showingSheet = false
     
     var body: some View {
-        VStack {
-            Text("Your name is \(user.firstName) \(user.lastName)")
-            
-            TextField("First Name", text: $user.firstName)
-            TextField("Last Name", text: $user.lastName)
+        
+        // button which when pressed, shows the second view
+        Button("Show Sheet") {
+            // step 2: toggling the state when button is tapped
+            showingSheet.toggle()
         }
-        .padding()
+        /* step 3: attaching the sheet somewhere in the current
+         view hierarchy */
+        .sheet(isPresented: $showingSheet) {
+            // step 4: showing the contents of the sheet
+            // invoking the second view
+            SecondView(name: "Abhimanyu")
+        }
     }
 }
 

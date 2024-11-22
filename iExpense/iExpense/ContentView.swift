070ca63c55337bc6eb9c562ac91289a6105c4be1
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2024-11-22 19:30:27 +0530

    Project 7 | iExpense | Storing user data with @AppStorage

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -8,17 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
-        /* setting the value for the property from the
-         UserDefaults corresponding to the key "Tap" */
+    @AppStorage("tapCount") private var tapCount = 0
     
     var body: some View {
         Button("Tap Count: \(tapCount)") {
             tapCount += 1
-            
-            UserDefaults.standard.set(tapCount, forKey: "Tap")
-            /* storing the value of tapCount in UserDefaults
-             against the key "Tap" */
         }
     }
 }

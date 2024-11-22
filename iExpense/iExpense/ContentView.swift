70f8387610ad36a058a548f37f15227209104cfe
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2024-11-22 19:04:28 +0530

    Project 7 | iExpense | Deleting List items using .onDelete()

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -7,40 +7,35 @@
 
 import SwiftUI
 
-// this is the second view that we want to show
-struct SecondView: View {
-    @Environment(\.dismiss) var dismiss
-        /* environment property to manually dismiss the second
-         view */
-    let name: String
+struct ContentView: View {
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
     
     var body: some View {
-        Text("Hello, \(name)!")
-        Button("Dismiss") {
-            dismiss() // calling the dismiss method
+        NavigationStack {
+            VStack {
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+                
+                Button("Add Number") {
+                    numbers.append(currentNumber)
+                    currentNumber += 1
+                }
+            }
+            // adding an edit toggle in the nav bar (optional)
+            .toolbar {
+                EditButton()
+            }
         }
     }
-}
-
-struct ContentView: View {
-    /* step 1: adding a state property to track whether the
-     sheet is showing or not */
-    @State private var showingSheet = false
     
-    var body: some View {
-        
-        // button which when pressed, shows the second view
-        Button("Show Sheet") {
-            // step 2: toggling the state when button is tapped
-            showingSheet.toggle()
-        }
-        /* step 3: attaching the sheet somewhere in the current
-         view hierarchy */
-        .sheet(isPresented: $showingSheet) {
-            // step 4: showing the contents of the sheet
-            // invoking the second view
-            SecondView(name: "Abhimanyu")
-        }
+    // method to delete rows from the list
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
     }
 }
 

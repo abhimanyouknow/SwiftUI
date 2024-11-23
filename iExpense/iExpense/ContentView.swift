ecd65707f48cfd72ac8347e28da0f78671393405
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2024-11-24 00:34:15 +0530

    Project 7 | iExpense | App Logic

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -7,24 +7,90 @@
 
 import SwiftUI
 
-struct User: Codable {
-    let firstName: String
-    let lastName: String
+struct ExpenseItem: Identifiable, Codable {
+    /* ExpenseItem conforming to Identifiable protocol signals
+     that ExpenseItem data can be identified uniquely, somehow.
+     
+     This has only one requirement, there must be a property
+     called 'id', with some unique identifier - could be a String,
+     Int or even a UUID
+     
+     As a result of this, we don't have to specify the id
+     parameter in the ForEach loop below */
+    var id = UUID() // this generated a new unique id for each instance
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+@Observable
+class Expenses {
+    var items = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+    
+    /* creating custom initializer to load data from UserDefaults
+     whenever user re-launches app */
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                /* [ExpenseItem].self is used to signal that
+                 we're refering to the type, i.e., an array of
+                 ExpenseItem */
+                items = decodedItems
+                return
+            }
+        }
+        
+        items = []
+    }
 }
 
 struct ContentView: View {
-    @State private var user = User(firstName: "Wayne", lastName: "Rooney")
+    @State private var expenses = Expenses()
+    
+    @State private var showingAddExpenses = false
     
     var body: some View {
-        Button("Save User") {
-            let encoder = JSONEncoder()
-            
-            if let userData = try? encoder.encode(user) {
-                UserDefaults.standard.set(userData, forKey: "UserData")
+        NavigationStack {
+            List {
+                ForEach(expenses.items) { item in
+                    HStack {
+                        VStack {
+                            Text(item.name)
+                                .font(.headline)
+                            
+                            Text(item.type)
+                        }
+                        
+                        Spacer()
+                        
+                        Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    }
+                }
+                .onDelete(perform: removeItems)
             }
-            
-            // NOTE: the data type of userData is Data
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button("Add Expense", systemImage: "plus") {
+                    showingAddExpenses = true
+                }
+            }
+            .sheet(isPresented: $showingAddExpenses) {
+                AddView(expenses: expenses) /* sharing the same
+                                             observable object
+                                             class between the
+                                             two views */
+            }
         }
+    }
+    
+    func removeItems(at offset: IndexSet) {
+        expenses.items.remove(atOffsets: offset)
     }
 }
 

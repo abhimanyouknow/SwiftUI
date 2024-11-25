bc37daa49f60805829f5ea93d39bf416f667cb90
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2024-11-25 11:52:57 +0530

    Project 8 | Moonshot | Laying out views in a scrolling grid

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -7,35 +7,18 @@
 
 import SwiftUI
 
-struct User: Codable {
-    let name: String
-    let address: Address
-}
-
-struct Address: Codable {
-    let street: String
-    let city: String
-}
-
 struct ContentView: View {
+    // step 1: defining layout for the data
+    let layout = [
+        GridItem(.adaptive(minimum: 80)) // adaptive col with min size 80
+    ]
     var body: some View {
-        Button("Decode JSON") {
-            // sample input data
-            let input = """
-            {
-                "name": "Wayne Rooney",
-                "address": {
-                    "street": "Old Trafford Road",
-                    "city": "Manchester"
+        ScrollView(.horizontal) {
+            // step 2: adding the LazyHGrid
+            LazyHGrid(rows: layout) {
+                ForEach(0..<1000) {
+                    Text("Item \($0)")
                 }
-            }
-            """
-            
-            let data = Data(input.utf8)
-            let decoder = JSONDecoder()
-            
-            if let user = try? decoder.decode(User.self, from: data) {
-                print(user.address.street)
             }
         }
     }

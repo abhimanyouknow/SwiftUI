1194e5866fe1e3a04794ecb38c5a716bbd663fe1
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2024-11-30 16:40:02 +0530

    Project 9 | Navigation | Programmatic Navigation with different data types using NavigationPath() data type

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -8,41 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    /* property to store the path for the navigation we're
-     working with */
-    @State private var path = [Int]()
+    @State private var path = NavigationPath()
     
     var body: some View {
-        /* binding nav stack's path to our property
-         any change to the path array becomes the active path
-         for our nav stack - since this is using two-way
-         binding, the path array stays updated (in sync) with
-         our view
-         */
         NavigationStack(path: $path) {
             VStack {
-                Button("Show 32") {
-                    path = [32]
-                    /* this will reset the nav stack, which
-                     will then navigatte to path 32 */
+                Button("Number 556") {
+                    path.append(556)
                 }
                 
-                Button("Show 64") {
-                    path.append(64)
-                    /* this will leave the existing nav stack
-                     as it is, and then navigate to path 64 */
-                }
-                
-                Button("Show 32 then 64") {
-                    path = [32, 64]
-                    /* this changes the whole array - meaning
-                     nav stack resets to root view, then
-                     navigates to path 32, then navigates to
-                     path 64 */
+                Button("String Hello") {
+                    path.append("Hello")
                 }
             }
             .navigationDestination(for: Int.self) { selection in
-                Text("Your selection: \(selection)")
+                Text("You selected the number: \(selection)")
+            }
+            .navigationDestination(for: String.self) { selection in
+                Text("You selected the string: \(selection)")
             }
         }
     }

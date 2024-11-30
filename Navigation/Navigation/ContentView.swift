a3f6e8f0e06cdc91262dfe69999ddb120e14f2ca
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2024-11-30 17:04:33 +0530

    Project 9 | Navigation | Programmatic Navigation to Root View using @Binding

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -7,26 +7,47 @@
 
 import SwiftUI
 
+struct DetailView: View {
+    var number: Int
+    @Binding var path: [Int] // Binding property for shared path
+    /* if we were using a NavigationPath property as path -
+     III -
+     
+     @Binding var path: NavigationPath
+     */
+
+    
+    var body: some View {
+        NavigationLink("Go to random number", value: Int.random(in: 1...1000))
+            .navigationTitle("Number: \(number)")
+            .toolbar {
+                Button("Home") {
+                    path.removeAll() /* using the shared path
+                                      property to reset the
+                                      array, this going back
+                                      to the root view */
+                    /* if we were using a NavigationPath
+                     property as path - II -
+                     
+                     path = NavigationPath()
+                     */
+                }
+            }
+    }
+}
+
 struct ContentView: View {
-    @State private var path = NavigationPath()
+    @State private var path = [Int]()
+    /* if we were using a NavigationPath property as path - I -
+     @State private var path = NavigationPath()
+     */
     
     var body: some View {
         NavigationStack(path: $path) {
-            VStack {
-                Button("Number 556") {
-                    path.append(556)
+            DetailView(number: 0, path: $path)
+                .navigationDestination(for: Int.self) { i in
+                    DetailView(number: i, path: $path)
                 }
-                
-                Button("String Hello") {
-                    path.append("Hello")
-                }
-            }
-            .navigationDestination(for: Int.self) { selection in
-                Text("You selected the number: \(selection)")
-            }
-            .navigationDestination(for: String.self) { selection in
-                Text("You selected the string: \(selection)")
-            }
         }
     }
 }

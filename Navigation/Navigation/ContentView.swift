bc94e2907f5f8197324e4edd987d13a9278a4d03
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2024-11-30 16:28:02 +0530

    Project 9 | Navigation | Programmatic Navigation using NavigationPath(path: )

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -7,28 +7,42 @@
 
 import SwiftUI
 
-// custom struct
-struct Student: Hashable {
-    var id = UUID()
-    var name: String
-    var age: Int
-}
-
 struct ContentView: View {
+    /* property to store the path for the navigation we're
+     working with */
+    @State private var path = [Int]()
+    
     var body: some View {
-        NavigationStack {
-            List(0..<100) { i in
-                // step 1
-                NavigationLink("Select \(i)", value: i)
+        /* binding nav stack's path to our property
+         any change to the path array becomes the active path
+         for our nav stack - since this is using two-way
+         binding, the path array stays updated (in sync) with
+         our view
+         */
+        NavigationStack(path: $path) {
+            VStack {
+                Button("Show 32") {
+                    path = [32]
+                    /* this will reset the nav stack, which
+                     will then navigatte to path 32 */
+                }
+                
+                Button("Show 64") {
+                    path.append(64)
+                    /* this will leave the existing nav stack
+                     as it is, and then navigate to path 64 */
+                }
+                
+                Button("Show 32 then 64") {
+                    path = [32, 64]
+                    /* this changes the whole array - meaning
+                     nav stack resets to root view, then
+                     navigates to path 32, then navigates to
+                     path 64 */
+                }
             }
-            // step 2
             .navigationDestination(for: Int.self) { selection in
-                Text("You selected \(selection)")
-            }
-            
-            // nav destination for Student
-            .navigationDestination(for: Student.self) { student in
-                Text("You selected \(student.name)")
+                Text("Your selection: \(selection)")
             }
         }
     }

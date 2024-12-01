8fb669fbf16d32cc99568d1edd69a780fce44d4e
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2024-12-01 14:47:06 +0530

    Project 9 | Navigation | Placing toolbar buttons in exact locations

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -10,24 +10,18 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationStack {
-            List(0..<100) {
-                Text("Row \($0)")
-            }
-            .navigationTitle("Title goes here")
-            .navigationBarTitleDisplayMode(.inline)
-                /* palces nav bar title in small text at the
-                 top-centre of the screen */
-            .toolbarBackground(.blue, for: .navigationBar)
-                /* the blue background of the nav title only
-                 appears when we scroll down a little bit */
-            .toolbarColorScheme(.dark, for: .navigationBar)
-                /* changes the text color of the title to
-                 white when user scrolls the list and the
-                 background turns blue */
-            .toolbar(.hidden, for: .navigationBar)
-                /* hiding the nav bar containing the title,
-                 while retaining the functionality of
-                 navigating to different views */
+            Text("Hello, world!")
+                .toolbar {
+                    ToolbarItemGroup(placement: .topBarLeading) {
+                        Button("Tap Me") {
+                            // button action code
+                        }
+                    
+                        Button("Or Tap Me") {
+                            // button action code
+                        }
+                    }
+                }
         }
     }
 }

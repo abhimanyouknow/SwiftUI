00f70de9565ca4d8369042a12af695f976ec6423
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2024-12-03 11:47:21 +0530

    PeerPact | Initial Commit

---
+++ PeerPact/PeerPact/PeerPactApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeerPactApp.swift
+//  PeerPact
+//
+//  Created by C3PO MBP on 03/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PeerPactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

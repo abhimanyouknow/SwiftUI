569353c8a33b36df21146dd9f43b9e509ee7e28d
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2024-12-06 11:48:42 +0530

    Project 10 | CupcakeCorner | Haptics - complex approach using CoreHaptics

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -5,15 +5,66 @@
 //  Created by C3PO MBP on 05/12/24.
 //
 
+import CoreHaptics
 import SwiftUI
 
 struct ContentView: View {
-    @State private var counter = 0
+    @State private var engine: CHHapticEngine?
+    
     var body: some View {
-        Button("Tap Count: \(counter)") {
-            counter += 1
+        Button("Play Haptic", action: complexSuccess)
+            .onAppear(perform: prepareHaptics)
+            /* it is important to start and prepare the
+             haptic engine when the view appears before we
+             attempt to trigger any haptics */
+    }
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        do {
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the Haptic Engien: \(error.localizedDescription)")
         }
-        .sensoryFeedback(.impact(weight: .heavy, intensity: 1), trigger: counter)
+    }
+    
+    func complexSuccess() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        var events = [CHHapticEvent]()
+        
+        // creating a ripple effect which gets stronger
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
+                /* .hapticTransient indicates a one-off
+                 haptic, as opposed to a continuous buzz */
+            
+            events.append(event)
+        }
+        
+        // creating a ripple effect which gets weaker
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
+                /* .hapticTransient indicates a one-off
+                 haptic, as opposed to a continuous buzz */
+            
+            events.append(event)
+        }
+
+        
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription)")
+        }
     }
 }
 

1f5c5e438ca63c7f0bd318aabde4a8db48d91457
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2024-12-09 12:09:16 +0530

    Project 11 | Bookworm | sample app for storing randomly generated names in a list using SwiftData

--- Bookworm/Bookworm/BookwormApp.swift
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -5,10 +5,9 @@
 //  Created by C3PO MBP on 09/12/24.
 //
 
-/* step 2: loading data model (tells the SwiftData to
- prepare some actual, physical storage on an iPhone to
- save data to, which is where it'll read & write our
- Studnet object */
+/* step 2: loading data model (tells the SwiftData to prepare some actual, physical
+ storage on an iPhone to save data to, which is where it'll read & write our Student
+ object) */
 
 import SwiftData // step 2 - part 1
 import SwiftUI
@@ -22,7 +21,7 @@ struct BookwormApp: App {
         .modelContainer(for: Student.self) // step 2 - part 2
         
         // step 3
-        /* setting up model context - which let's us work with all our data in Memory
+        /* setting up model context - which lets us work with all our data in Memory
          (RAM). this is done automatically for us by the .modelContainer(for: ) modifier -
          called the main context & is stored inside the SwiftUI environment */
     }
--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -5,12 +5,48 @@
 //  Created by C3PO MBP on 09/12/24.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+        /* we're reading out from the environment and the
+         modelContainer modifier from earlier place the main
+         context in there for us to work with */
+    @Query var students: [Student]
+        /* the @Query property wrapper tells swift to look for
+         the main context based on what was placed into the
+         environment earlier, then queries the container
+         through that main context and finds it. we get all the
+         Student data from memory here, and it'll watch them
+         for updates so if the array changes, it'll re-invoke
+         the body property & make the view update as well */
     
     var body: some View {
-        Text("Hello, world!")
+        NavigationStack {
+            List(students) { student in
+                Text(student.name)
+            }
+            .navigationTitle("Classroom")
+            .toolbar {
+                /* button that generates random student info
+                 and saves them in our modelContext */
+                Button("Add") {
+                    let firstNames = ["Ginny", "Harry", "Herminoe", "Luna", "Ron"]
+                    let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
+                    
+                    let chosenFirstName = firstNames.randomElement()!
+                    let chosenLastName = lastNames.randomElement()!
+                    
+                    let student = Student(id: UUID(), name: "\(chosenFirstName) \(chosenLastName)")
+                    
+                    /* asking our modelContext to add this
+                     student to our storage - which means it'll
+                     be queued up for saving asap */
+                    modelContext.insert(student)
+                }
+            }
+        }
     }
 }
 

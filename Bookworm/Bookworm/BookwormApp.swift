64daea2f99e868c055c7968569998885b7e12ecb
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2024-12-09 12:10:29 +0530

    Project 11 | Bookworm | reset app for Bookworm project

--- Bookworm/Bookworm/BookwormApp.swift
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -5,10 +5,6 @@
 //  Created by C3PO MBP on 09/12/24.
 //
 
-/* step 2: loading data model (tells the SwiftData to prepare some actual, physical
- storage on an iPhone to save data to, which is where it'll read & write our Student
- object) */
-
 import SwiftData // step 2 - part 1
 import SwiftUI
 
@@ -18,11 +14,5 @@ struct BookwormApp: App {
         WindowGroup {
             ContentView()
         }
-        .modelContainer(for: Student.self) // step 2 - part 2
-        
-        // step 3
-        /* setting up model context - which lets us work with all our data in Memory
-         (RAM). this is done automatically for us by the .modelContainer(for: ) modifier -
-         called the main context & is stored inside the SwiftUI environment */
     }
 }

31ab15df5280bf4e4a92296bdf39106d392ee7d3
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2024-12-09 11:15:26 +0530

    Project 11 | Bookworm | accepting multi-line text input usnig TextEditor

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -7,43 +7,19 @@
 
 import SwiftUI
 
-struct PushButton: View {
-    let title: String
-    @Binding var isOn: Bool
-        /* had we used @State, then isOn would have gotten
-         the initial value from the ContentView struct
-         property 'rememberMe', but then PushButton view
-         struct would have toggled the value of isOn in
-         isolation to rememberMe.
-         
-         since we want both properties, rememberMe and isOn
-         to reflect the same boolean value, we're using the
-         @Binding property wrapper - now there is a two-way
-         binding between rememberMe & isOn across the two
-         views.
-         */
-    
-    var onColors = [Color.red, Color.yellow]
-    var offColors = [Color(white: 0.6), Color(white: 0.4)]
-    
-    var body: some View {
-        Button(title) {
-            isOn.toggle()
-        }
-        .padding()
-        .background(LinearGradient(colors: isOn ? onColors : offColors, startPoint: .top, endPoint: .bottom))
-        .foregroundStyle(.white)
-        .clipShape(.capsule)
-        .shadow(radius: isOn ? 0 : 5)
-    }
-}
-
 struct ContentView: View {
-    @State private var rememberMe = false
+    @AppStorage("notes") private var notes = ""
+        /* @AppStorage is basically using UserDefaults for
+         storing simple things, but this should NOT be used
+         to store secure information (usernames, passwords,
+         etc.) */
     
     var body: some View {
-        PushButton(title: "Remember Me", isOn: $rememberMe)
-        Text(rememberMe ? "On" : "Off")
+        NavigationStack {
+            TextEditor(text: $notes)
+                .navigationTitle("Notes")
+                .padding()
+        }
     }
 }
 

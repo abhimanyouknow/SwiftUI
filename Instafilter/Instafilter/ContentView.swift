7196d27d32efa21b93a2cf9b7c1b33ba671b469b
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2024-12-18 11:44:57 +0530

    Project 13 | Instafilter | showing multiple options with .confirmationDialog()

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -8,21 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var blurAmount = 0.0
+    @State private var showingConfirmation = false
+    @State private var backgroundColor = Color.white
     
     var body: some View {
-        VStack {
-            Text("Hello, world!")
-                .blur(radius: blurAmount)
+        Button("Tap me") {
+            showingConfirmation.toggle()
+        }
+        .frame(width: 300, height: 300)
+        .background(backgroundColor)
+        .confirmationDialog("Change background", isPresented: $showingConfirmation) {
+            Button("Red") { backgroundColor = .red }
+            Button("Green") { backgroundColor = .green }
+            Button("Yellow") { backgroundColor = .yellow }
             
-            Slider(value: $blurAmount, in: 0...20)
-                .onChange(of: blurAmount) { oldValue, newValue in
-                    print("New Value: \(newValue)")
-                }
-            
-            Button("Random Blur") {
-                blurAmount = Double.random(in: 0...20)
-            }
+            Button("Cancel", role: .cancel) { }
+        } message: {
+            Text("Select a new background color")
         }
     }
 }

7cc12b0cf9c59050ebe1487d0db64a321ee589ad
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2024-12-18 11:32:33 +0530

    Project 13 | Instafilter | responding to state changes using onChange()

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -8,14 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var blurAmount = 0.0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
             Text("Hello, world!")
+                .blur(radius: blurAmount)
+            
+            Slider(value: $blurAmount, in: 0...20)
+                .onChange(of: blurAmount) { oldValue, newValue in
+                    print("New Value: \(newValue)")
+                }
+            
+            Button("Random Blur") {
+                blurAmount = Double.random(in: 0...20)
+            }
         }
-        .padding()
     }
 }
 

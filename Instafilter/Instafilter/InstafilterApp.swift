ce1fb0b748e975398d160db14e6b0595296f428c
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2024-12-18 11:32:06 +0530

    Project 13 | Instafilter | initial commit

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by C3PO MBP on 18/12/24.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

1ad9c4b2880552cd7097214d0c076ce23616e1fc
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2024-12-20 00:25:25 +0530

    Project 13 | Instafilter | Integrating CoreImage with SwiftUI

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -5,27 +5,42 @@
 //  Created by C3PO MBP on 18/12/24.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingConfirmation = false
-    @State private var backgroundColor = Color.white
+    @State private var image: Image?
     
     var body: some View {
-        Button("Tap me") {
-            showingConfirmation.toggle()
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
         }
-        .frame(width: 300, height: 300)
-        .background(backgroundColor)
-        .confirmationDialog("Change background", isPresented: $showingConfirmation) {
-            Button("Red") { backgroundColor = .red }
-            Button("Green") { backgroundColor = .green }
-            Button("Yellow") { backgroundColor = .yellow }
-            
-            Button("Cancel", role: .cancel) { }
-        } message: {
-            Text("Select a new background color")
-        }
+        .onAppear(perform: loadImage)
+    }
+    
+    func loadImage() {
+        // step 1: loading our image into a UIImage
+        let inputImage = UIImage(resource: .example)
+        
+        // step 2: putting this image recipe into a CIImage
+        let beginImage = CIImage(image: inputImage)
+        
+        // step 3: making a CoreImage context & a CoreImage filter
+        let context = CIContext()
+        let currentFilter = CIFilter.sepiaTone()
+        
+        // passing in two parameters to currentFilter - the image we want to apply the filter to, and the intensity
+        currentFilter.inputImage = beginImage
+        currentFilter.intensity = 1 // max intensity
+        
+        // now we convert the output from our filter to a SwiftUI image we can display in our view
+        guard let outputImage = currentFilter.outputImage else { return }
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+        let uiImage = UIImage(cgImage: cgImage)
+        image = Image(uiImage: uiImage)
     }
 }
 

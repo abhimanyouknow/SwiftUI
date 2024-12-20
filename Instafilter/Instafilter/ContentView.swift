c507d7d066ebc53a74d89a0f7dcf5a7cfaec7d9b
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2024-12-20 10:28:59 +0530

    Project 13 | Instafilter | Selecting anc loading a single image from users' library

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -5,21 +5,30 @@
 //  Created by C3PO MBP on 18/12/24.
 //
 
-import CoreImage
-import CoreImage.CIFilterBuiltins
+// step 1
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
+    // step 2
+    @State private var pickerItem: PhotosPickerItem?
+    @State private var selectedImage: Image?
+    
     var body: some View {
-        ContentUnavailableView {
-            Label("No Snippets", systemImage: "swift")
-        } description: {
-            Text("You don't have any saved snippets yet")
-        } actions: {
-            Button("Create Snippet") {
-                // some code
+        VStack {
+            // step 3
+            PhotosPicker("Select a picture", selection: $pickerItem, matching: .images)
+            
+            // step 5
+            selectedImage?
+                .resizable()
+                .scaledToFit()
+        }
+        // step 4
+        .onChange(of: pickerItem) {
+            Task {
+                selectedImage = try await pickerItem?.loadTransferable(type: Image.self)
             }
-            .buttonStyle(.borderedProminent)
         }
     }
 }

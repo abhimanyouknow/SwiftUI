1e05b44d8f1c4b99628b2d8fdccb6754f14a46c6
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2024-12-20 11:41:21 +0530

    Project 13 | Instafilter | Asking user to leave an App Store review

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -5,12 +5,18 @@
 //  Created by C3PO MBP on 18/12/24.
 //
 
+// step 1
+import StoreKit
 import SwiftUI
 
 struct ContentView: View {
+    // step 2: add a property to read review requester from SwiftUI's environment
+    @Environment(\.requestReview) var requestReview
+    
     var body: some View {
-        ShareLink(item: URL(string: "https://www.hackingwithswift.com")!, subject: Text("Lear Swift here"), message: Text("Check out the 100 days of SwiftUI")) {
-            Label("Spread the word about Swift", systemImage: "swift")
+        // step 3: request the review when you're ready
+        Button("Rate the app") {
+            requestReview()
         }
     }
 }

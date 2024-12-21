3c21e81cca81d889bdaa1855e5acb0a2a94cb8d4
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2024-12-21 14:45:39 +0530

    Project 13 | Instalfilter | reset code

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -5,19 +5,11 @@
 //  Created by C3PO MBP on 18/12/24.
 //
 
-// step 1
-import StoreKit
 import SwiftUI
 
 struct ContentView: View {
-    // step 2: add a property to read review requester from SwiftUI's environment
-    @Environment(\.requestReview) var requestReview
-    
     var body: some View {
-        // step 3: request the review when you're ready
-        Button("Rate the app") {
-            requestReview()
-        }
+        Text("Hello, world!")
     }
 }
 

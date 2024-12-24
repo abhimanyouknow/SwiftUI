1c2ff12c80c35e5330ef83e8df8f094c6c930973
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2024-12-24 11:23:56 +0530

    Project 14 | BucketList | adding conformance to Comparable for custom types

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -7,15 +7,32 @@
 
 import SwiftUI
 
+// step 1: add Comparable protocol to the User struct
+struct User: Comparable, Identifiable {
+    let id = UUID()
+    var firstName: String
+    var lastName: String
+    
+    // step 2: add method "<" for comparison (Operator Overloading)
+    static func <(lhs: User, rhs: User) -> Bool {
+        lhs.lastName < rhs.lastName
+    }
+}
+
 struct ContentView: View {
+    let users = [
+        User(firstName: "Wayne", lastName: "Rooney"),
+        User(firstName: "Robin", lastName: "van Persie"),
+        User(firstName: "Rio", lastName: "Ferdinand")
+    ].sorted()
+        /* since we used operator overloaing for the
+         less-than operator (<), we can directly call the
+         .sorted() function */
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(users) { user in
+            Text("\(user.lastName), \(user.firstName)")
         }
-        .padding()
     }
 }
 

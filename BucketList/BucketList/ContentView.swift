8d78051f5a703b33fcc46c4594dddc0a8f6f75ac
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2024-12-25 13:23:02 +0530

    Project 14 | BucketList | MapKit with SwuiftUI - adding a satellite type map with street view & 3d elevation data

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -5,53 +5,14 @@
 //  Created by C3PO MBP on 24/12/24.
 //
 
+import MapKit
 import SwiftUI
 
-struct LoadingView: View {
-    var body: some View {
-        Text("Loading...")
-    }
-}
-
-struct SuccessView: View {
-    var body: some View {
-        Text("Success..")
-    }
-}
-
-struct FailureView: View {
-    var body: some View {
-        Text("Failure...")
-    }
-}
-
 struct ContentView: View {
-    enum LoadingState {
-        case loading, success, failure
-    }
-    
-    @State private var loadingState = LoadingState.loading
     
     var body: some View {
-        switch loadingState {
-        case .loading:
-            LoadingView()
-        case .success:
-            SuccessView()
-        case .failure:
-            FailureView()
-        }
-        
-        /* we could also have used an if-else block -
-         
-        if loadingState == .loading {
-            LoadingView()
-        } else if loadingState == .success {
-            SuccessView()
-        } else if loadingState == .failure {
-            FailureView()
-        }
-         */
+        Map()
+            .mapStyle(.hybrid(elevation: .realistic))
     }
 }
 

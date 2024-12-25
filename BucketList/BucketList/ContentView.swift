be71b248d3b126f0283b3c835cf89b0992a952fe
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2024-12-25 13:43:46 +0530

    Project 14 | BucketList | MapKit with SwuiftUI - two way binding the position so that user can change it using buttons

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -10,14 +10,32 @@ import SwiftUI
 
 struct ContentView: View {
     // setting london as a position
-    let position = MapCameraPosition.region(
+    @State private var position = MapCameraPosition.region(
         MKCoordinateRegion(
             center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
             span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)))
     
     var body: some View {
-        Map(initialPosition: position)
-            .mapStyle(.hybrid(elevation: .realistic))
+        VStack {
+            Map(position: $position)
+                .mapStyle(.hybrid(elevation: .realistic))
+            
+            HStack {
+                Button("Paris") {
+                    position = MapCameraPosition.region(
+                        MKCoordinateRegion(
+                            center: CLLocationCoordinate2D(latitude: 48.8566, longitude: 2.3522),
+                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)))
+                }
+                
+                Button("Tokyo") {
+                    position = MapCameraPosition.region(
+                        MKCoordinateRegion(
+                            center: CLLocationCoordinate2D(latitude: 35.6897, longitude: 139.6922),
+                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)))
+                }
+            }
+        }
     }
 }
 

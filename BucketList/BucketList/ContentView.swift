8c731a9c4aedd18236de63b3f06a4f0d4d1398da
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2024-12-25 13:36:31 +0530

    Project 14 | BucketList | MapKit with SwuiftUI - setting the initial position of the map (as london in this case)

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -9,8 +9,14 @@ import MapKit
 import SwiftUI
 
 struct ContentView: View {
+    // setting london as a position
+    let position = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
+            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)))
+    
     var body: some View {
-        Map(interactionModes: [.rotate, .zoom])
+        Map(initialPosition: position)
             .mapStyle(.hybrid(elevation: .realistic))
     }
 }

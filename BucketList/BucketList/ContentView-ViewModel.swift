457b8817e2982a713512ce0e033b86491cc51889
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2024-12-28 16:43:13 +0530

    Project 14 | BucketList | app logic - using MVVM

---
+++ BucketList/BucketList/ContentView-ViewModel.swift
@@ -0,0 +1,58 @@
+//
+//  ContentView-ViewModel.swift
+//  BucketList
+//
+//  Created by C3PO MBP on 28/12/24.
+//
+
+/* we're gonna use this file to make a new class that manages our data for us and
+ manipulates it on behalf of ContentView */
+
+import CoreLocation
+import Foundation
+import MapKit
+
+extension ContentView {
+    @Observable
+    class ViewModel {
+        private(set) var locations: [Location]
+        var selectedPlace: Location?
+        
+        let savePath = URL.documentsDirectory.appending(path: "SavedPlaces")
+        
+        init() {
+            do {
+                let data = try Data(contentsOf: savePath)
+                locations = try JSONDecoder().decode([Location].self, from: data)
+            } catch {
+                locations = []
+            }
+        }
+        
+        func save() {
+            do {
+                let data = try JSONEncoder().encode(locations)
+                try data.write(to: savePath, options: [.atomic, .completeFileProtection])
+            } catch {
+                print("Unable to save data.")
+            }
+        }
+        
+        func addLocation(at point: CLLocationCoordinate2D) {
+            let newLocation = Location(id: UUID(), name: "New Location", description: "", latitude: point.latitude, longitude: point.longitude)
+            
+            locations.append(newLocation)
+            save()
+        }
+        
+        func updateLocation(location: Location) {
+            guard let selectedPlace else { return }
+            
+            if let index = locations.firstIndex(of: selectedPlace) {
+                locations[index] = location
+                save()
+            }
+        }
+        
+    }
+}

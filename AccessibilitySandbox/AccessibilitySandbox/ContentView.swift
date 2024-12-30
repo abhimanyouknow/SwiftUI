d189760a4c8103c31f1f98705ccccde7d2aca6a7
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2024-12-30 12:20:56 +0530

    Project 15 | AccessibilitySandbox | grouping several views as one

--- AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
+++ AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
@@ -25,8 +25,14 @@ struct ContentView: View {
     @State private var selectedPicture = Int.random(in: 0...3)
     
     var body: some View {
-        Image(.character)
-            .accessibilityHidden(true)
+        VStack {
+            Text("Your score is")
+            
+            Text("1000")
+                .font(.title)
+        }
+        .accessibilityElement(children: .ignore)
+        .accessibilityLabel("Your score is 1000")
     }
 }
 

db17cc43642d5a09daa35a5a587d7c70d748ffa9
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2024-12-30 09:55:57 +0530

    Project 15 | AccessibilitySandbox | initial commit

---
+++ AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by C3PO MBP on 30/12/24.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

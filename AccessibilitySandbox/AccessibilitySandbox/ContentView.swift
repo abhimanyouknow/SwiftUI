ba94a634dfeebb6ebb5352961d1b65e27a9daf96
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2024-12-30 12:38:23 +0530

    Project 15 | AccessibilitySandbox | reading the value of controls

--- AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
+++ AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
@@ -8,31 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
-    let pictures = [
-        "ales-krivec-15949",
-        "galina-n-189483",
-        "kevin-horstmann-141705",
-        "nicolas-tissot-335096"
-    ]
-    
-    let labels = [
-        "Tulips",
-        "Frozen tree buds",
-        "Sunflowers",
-        "Fireworks"
-    ]
-    
-    @State private var selectedPicture = Int.random(in: 0...3)
+    @State private var value = 10
     
     var body: some View {
         VStack {
-            Text("Your score is")
+            Text("Value: \(value)")
             
-            Text("1000")
-                .font(.title)
+            Button("Increment") {
+                value += 1
+            }
+            
+            Button("Decrement") {
+                value -= 1
+            }
         }
-        .accessibilityElement(children: .ignore)
-        .accessibilityLabel("Your score is 1000")
+        .accessibilityElement()
+        .accessibilityLabel("Value")
+        .accessibilityValue(String(value))
+        .accessibilityAdjustableAction() { direction in
+            switch direction {
+            case .increment:
+                value += 1
+            case .decrement:
+                value -= 1
+            default:
+                print("Not handled")
+            }
+        }
     }
 }
 

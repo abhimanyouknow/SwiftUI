668562e3bd445589b240af4d8b18ac8327483b40
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2024-12-31 10:54:25 +0530

    Project 15 | AccessibilitySandbox | fixing Project 2, GuessTheFlag, by using .accessibilityLabel()

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -42,6 +42,21 @@ struct ContentView: View {
     @State private var opacityAmount = [1.0, 1.0, 1.0]
     @State private var scaleAmount = [1.0, 1.0, 1.0]
     
+    // project 15 - part 1
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes. Top stripe blue, middle stripe black, bottom stripe white.",
+        "France": "Flag with three vertical stripes. Left stripe blue, middle stripe white, right stripe red.",
+        "Germany": "Flag with three horizontal stripes. Top stripe black, middle stripe red, bottom stripe gold.",
+        "Ireland": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe orange.",
+        "Italy": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe red.",
+        "Nigeria": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe green.",
+        "Poland": "Flag with two horizontal stripes. Top stripe white, bottom stripe red.",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red.",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background.",
+        "Ukraine": "Flag with two horizontal stripes. Top stripe blue, bottom stripe yellow.",
+        "US": "Flag with many red and white stripes, with white stars on a blue background in the top-left corner."
+    ]
+    
     var body: some View {
         ZStack {
             RadialGradient(stops: [
@@ -87,6 +102,8 @@ struct ContentView: View {
                         // project 6 - challenge 3 - part 3
                         .scaleEffect(scaleAmount[number])
                         .animation(.default, value: scaleAmount[number])
+                        // project 15 - part 2
+                        .accessibilityLabel(labels[countries[number], default: "Unknown flag"])
                     }
                 }
                 .frame(maxWidth: .infinity)

e1c216e0e96e5b4f4bb071fb94018aef92d8f651
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2025-01-04 15:38:09 +0530

    Project 16 | HotProspects | creating tabs using a simple TabView

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -8,26 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    let users = ["Tohru", "Yuki", "Kyo", "Momiji"]
-    
-    /* step 1 - make a state property to store whatever was tapped, this
-     will be an empty set of String in our case because nothing is
-     selected by default */
-    @State private var selection = Set<String>()
-    
     var body: some View {
-        // step 2 - tell our list to update selection when a row is tapped
-        List(users, id: \.self, selection: $selection) { user in
-            Text(user)
+        TabView {
+            Text("Tab 1")
+                .tabItem {
+                    Label("One", systemImage: "star")
+                }
+            
+            Text("Tab 2")
+                .tabItem {
+                    Label("Two", systemImage: "circle")
+                }
         }
-        
-        // step 3 - use the selection somehow
-        if selection.isEmpty == false {
-            Text("You selected: \(selection.formatted())")
-        }
-        
-        // step 4 - adding EditButton() to trigger multi-select mode
-        EditButton()
     }
 }
 

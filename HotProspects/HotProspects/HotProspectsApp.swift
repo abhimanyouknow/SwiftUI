e757510d9c1a3ab58af2e19166efb27f6a1a55c4
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2025-01-04 10:28:43 +0530

    Project 16 | HotProspects | initial commit

---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by C3PO MBP on 04/01/25.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

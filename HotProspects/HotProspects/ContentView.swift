bc806287fba1f71697087173baac2d98cedd33ba
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2025-01-04 15:33:36 +0530

    Project 16 | HotProspects | letting users select multiple items from a list

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -11,9 +11,9 @@ struct ContentView: View {
     let users = ["Tohru", "Yuki", "Kyo", "Momiji"]
     
     /* step 1 - make a state property to store whatever was tapped, this
-     will be an optional string in our case because nothing is selected
-     by default */
-    @State private var selection: String?
+     will be an empty set of String in our case because nothing is
+     selected by default */
+    @State private var selection = Set<String>()
     
     var body: some View {
         // step 2 - tell our list to update selection when a row is tapped
@@ -22,9 +22,12 @@ struct ContentView: View {
         }
         
         // step 3 - use the selection somehow
-        if let selection {
-            Text("You selected: \(selection)")
+        if selection.isEmpty == false {
+            Text("You selected: \(selection.formatted())")
         }
+        
+        // step 4 - adding EditButton() to trigger multi-select mode
+        EditButton()
     }
 }
 

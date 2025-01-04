df649c19c44632c9c4cb83e73305b4abee175862
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2025-01-04 16:02:15 +0530

    Project 16 | HotProspects | controlling the current tab view being shown programmatically

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -8,17 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    // step 1 - @State property to track the tab that's currently showing
+    @State private var selectedTab = "One"
+    
     var body: some View {
-        TabView {
-            Text("Tab 1")
-                .tabItem {
-                    Label("One", systemImage: "star")
-                }
+        // step 3 - pass that as a binding into the TabView so that it is tracked automatically
+        TabView(selection: $selectedTab) {
+            Button("Show Tab 2") {
+                // step 2 - modify the property to a new value when you want to change tabs
+                selectedTab = "Two"
+            }
+            .tabItem {
+                Label("One", systemImage: "star")
+            }
+            // step 4 - tell SwiftUI which tab is to be shown for each value of that property
+            .tag("One")
             
             Text("Tab 2")
                 .tabItem {
                     Label("Two", systemImage: "circle")
                 }
+                .tag("Two")
         }
     }
 }

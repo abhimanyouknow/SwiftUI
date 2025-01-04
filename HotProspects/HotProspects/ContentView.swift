e85b86ea2461f7199d4cc10732abe382b02d67e9
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2025-01-04 15:27:49 +0530

    Project 16 | HotProspects | letting users select a single item from a list

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    let users = ["Tohru", "Yuki", "Kyo", "Momiji"]
+    
+    /* step 1 - make a state property to store whatever was tapped, this
+     will be an optional string in our case because nothing is selected
+     by default */
+    @State private var selection: String?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        // step 2 - tell our list to update selection when a row is tapped
+        List(users, id: \.self, selection: $selection) { user in
+            Text(user)
         }
-        .padding()
+        
+        // step 3 - use the selection somehow
+        if let selection {
+            Text("You selected: \(selection)")
+        }
     }
 }
 

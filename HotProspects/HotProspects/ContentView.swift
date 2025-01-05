218c1ee9729e324820c281ace08cc251c98a77bc
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2025-01-05 13:08:58 +0530

    Project 16 | HotProspects | creating context menus

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -8,30 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var output = ""
+    @State private var backgroundColor = Color.red
     
     var body: some View {
-        Text(output)
-            .task {
-                await fetchReadings()
-            }
-    }
-    
-    func fetchReadings() async {
-        let fetchTask = Task {
-            let url = URL(string: "https://hws.dev/readings.json")!
-            let (data, _) = try await URLSession.shared.data(from: url)
-            let readings = try JSONDecoder().decode([Double].self, from: data)
-            return "Found \(readings.count) readings"
-        }
-        
-        let result = await fetchTask.result
-        
-        switch result {
-        case .success(let str):
-            output = str
-        case .failure(let error):
-            output = "Error: \(error.localizedDescription)"
+        VStack {
+            Text("Hello, world!")
+                .padding()
+                .foregroundStyle(.white)
+                .background(backgroundColor)
+            
+            Text("Change color")
+                .padding()
+                .contextMenu {
+                    Button("Red", systemImage: "circle") {
+                        backgroundColor = .red
+                    }
+                    
+                    Button("Green", systemImage: "square") {
+                        backgroundColor = .green
+                    }
+                    
+                    Button("Blue", systemImage: "triangle") {
+                        backgroundColor = .blue
+                    }
+                }
         }
     }
 }

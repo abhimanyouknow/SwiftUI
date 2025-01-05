5e8c251136a06d1debe4f76e8fdf2d4d3d6f8681
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2025-01-05 12:39:09 +0530

    Project 16 | HotProspects | understanding swift's Result type

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -8,27 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    // step 1 - @State property to track the tab that's currently showing
-    @State private var selectedTab = "One"
+    @State private var output = ""
     
     var body: some View {
-        // step 3 - pass that as a binding into the TabView so that it is tracked automatically
-        TabView(selection: $selectedTab) {
-            Button("Show Tab 2") {
-                // step 2 - modify the property to a new value when you want to change tabs
-                selectedTab = "Two"
+        Text(output)
+            .task {
+                await fetchReadings()
             }
-            .tabItem {
-                Label("One", systemImage: "star")
-            }
-            // step 4 - tell SwiftUI which tab is to be shown for each value of that property
-            .tag("One")
-            
-            Text("Tab 2")
-                .tabItem {
-                    Label("Two", systemImage: "circle")
-                }
-                .tag("Two")
+    }
+    
+    func fetchReadings() async {
+        let fetchTask = Task {
+            let url = URL(string: "https://hws.dev/readings.json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let readings = try JSONDecoder().decode([Double].self, from: data)
+            return "Found \(readings.count) readings"
+        }
+        
+        let result = await fetchTask.result
+        
+        do {
+            output = try result.get()
+        } catch {
+            output = "Error: \(error.localizedDescription)"
         }
     }
 }

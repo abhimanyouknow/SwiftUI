1cfd1a9902da1ff165291f801403e13283c31d28
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2025-01-05 13:13:11 +0530

    Project 16 | HotProspects | controlling image interpolation

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -8,31 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var backgroundColor = Color.red
-    
     var body: some View {
-        VStack {
-            Text("Hello, world!")
-                .padding()
-                .foregroundStyle(.white)
-                .background(backgroundColor)
-            
-            Text("Change color")
-                .padding()
-                .contextMenu {
-                    Button("Red", systemImage: "circle") {
-                        backgroundColor = .red
-                    }
-                    
-                    Button("Green", systemImage: "square") {
-                        backgroundColor = .green
-                    }
-                    
-                    Button("Blue", systemImage: "triangle") {
-                        backgroundColor = .blue
-                    }
-                }
-        }
+        Image(.example)
+            .interpolation(.none)
+            .resizable()
+            .scaledToFit()
+            .background(.black)
     }
 }
 

19304e695217ee133b8c6f6a488e56ad9d5a46ea
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2025-01-07 09:41:30 +0530

    Project 16 | HotProspects | reset

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -5,22 +5,11 @@
 //  Created by C3PO MBP on 04/01/25.
 //
 
-import SamplePackage
 import SwiftUI
 
 struct ContentView: View {
-    let possibleNumbers = 1...60
-    
-    var result: String {
-        let selected = possibleNumbers.random(7).sorted()
-            /* we're using the .random() method extension from the
-             SamplePackage that we imported */
-        let strings = selected.map(String.init)
-        return strings.formatted()
-    }
-    
     var body: some View {
-        Text(result)
+        Text("Hello, world!")
     }
 }
 

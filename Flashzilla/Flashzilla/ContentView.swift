ce79a5603f51ccadc9f96edbe44f420f69c60167
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2025-01-11 18:08:14 +0530

    Project 17 | Flashzilla | gestures - overriding default gesture precedence and firing both parent & child view gestures at the same time with .simultaneousGesture()

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
                     print("Text tapped")
                 }
         }
-        .highPriorityGesture(
+        .simultaneousGesture(
             TapGesture()
                 .onEnded {
                     print("VStack tapped")

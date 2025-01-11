74dc2706074c10f9a382b2e120bf56263fca8682
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2025-01-11 18:02:43 +0530

    Project 17 | Flashzilla | gestures - default gesture precedence for parent and child components

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -8,25 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var currentAmount = Angle.zero
-    @State private var finalAmount = Angle.zero
-    
     var body: some View {
         VStack {
             Text("Hello, world!")
-                .rotationEffect(currentAmount + finalAmount)
-                .gesture(
-                    RotateGesture()
-                        .onChanged { value in
-                            currentAmount = value.rotation
-                        }
-                        .onEnded { value in
-                            finalAmount += currentAmount
-                            currentAmount = .zero
-                        }
-                )
+                .onTapGesture {
+                    print("Text tapped")
+                }
         }
-        .padding()
+        .onTapGesture {
+            print("VStack tapped")
+        }
     }
 }
 

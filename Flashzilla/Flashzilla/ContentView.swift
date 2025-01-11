930e6472b81a790b3af3ca8152a77c44c931b2c4
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2025-01-11 14:26:09 +0530

    Project 17 | Flashzilla | gestures - magnifyGesture with .onChanged and .onEnded closures

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var currentAmount = 0.0
+    @State private var finalAmount = 1.0
+    
     var body: some View {
         VStack {
             Text("Hello, world!")
-                .onLongPressGesture(minimumDuration: 2) {
-                    print("Double tapped")
-                } onPressingChanged: { inProgress in
-                    print("In progress: \(inProgress)")
-                }
+                .scaleEffect(finalAmount + currentAmount)
+                .gesture(
+                    MagnifyGesture()
+                        .onChanged { value in
+                            currentAmount = value.magnification - 1
+                        }
+                        .onEnded { value in
+                            finalAmount += currentAmount
+                            currentAmount = 0
+                        }
+                )
         }
         .padding()
     }

ddf6ca77a62454b0829991c7a2c140d51879a683
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2025-01-11 11:38:32 +0530

    Project 17 | Flashzilla | initial commit

---
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Flashzilla
+//
+//  Created by C3PO MBP on 11/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

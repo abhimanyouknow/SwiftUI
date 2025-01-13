5b0849ad5daf2e035b6f76f2fe89685d0d3355b4
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2025-01-13 12:25:19 +0530

    Project 17 | Flashzilla | app logic 1 - adding a stack of cards, removing them with drag gesture

---
+++ Flashzilla/Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by C3PO MBP on 13/01/25.
+//
+
+import Foundation
+
+struct Card {
+    var prompt: String
+    var answer: String
+    
+    static let example = Card(prompt: "Who played the 13th doctor in Doctor Who?", answer: "Jodie Whittaker")
+}

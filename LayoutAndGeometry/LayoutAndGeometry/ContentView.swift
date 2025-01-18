fbf6c0b2a74b80c050c76dbd5db9867c5fc6798a
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2025-01-18 14:27:21 +0530

    Project 18 | LayoutAndGeometry | GeometryReader - image to take up 80% width of container view

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -7,39 +7,13 @@
 
 import SwiftUI
 
-extension VerticalAlignment {
-    enum MidAccountAndName: AlignmentID {
-        static func defaultValue(in context: ViewDimensions) -> CGFloat {
-            context[.top]
-        }
-    }
-    
-    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
-}
-
 struct ContentView: View {
     var body: some View {
-        HStack(alignment: .midAccountAndName) {
-            VStack {
-                Text("@abhimanyouknow")
-                    .alignmentGuide(.midAccountAndName) { d in
-                        d[VerticalAlignment.center]
-                    }
-                
-                Image(.abhimanyu)
-                    .resizable()
-                    .frame(width: 64, height: 84)
-            }
-            
-            VStack {
-                Text("Name:")
-                
-                Text("Abhimanyu")
-                    .alignmentGuide(.midAccountAndName) { d in
-                        d[VerticalAlignment.center]
-                    }
-                    .font(.largeTitle)
-            }
+        GeometryReader { proxy in
+            Image(.abhimanyu)
+                .resizable()
+                .scaledToFit()
+                .frame(width: proxy.size.width * 0.8)
         }
     }
 }

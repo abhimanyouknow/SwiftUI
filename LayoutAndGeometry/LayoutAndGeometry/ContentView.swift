399184f94c81e0d520ffa282dab635e24a2c792b
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2025-01-18 14:42:33 +0530

    Project 18 | LayoutAndGeometry | GeometryReader - instance where .containerRelativeFrame() would not work + centering content inside GeometryReader

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,11 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        GeometryReader { proxy in
-            Image(.abhimanyu)
-                .resizable()
-                .scaledToFit()
-                .frame(width: proxy.size.width * 0.8)
+        HStack {
+            Text("IMPORTANT")
+                .frame(width: 200)
+                .background(.blue)
+            
+            GeometryReader { proxy in
+                Image(.example)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: proxy.size.width * 0.8)
+                    .frame(width: proxy.size.width, height: proxy.size.height) // for centering the image
+            }
         }
     }
 }

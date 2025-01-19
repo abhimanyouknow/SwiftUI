10c88e58d3409c50b4c009db38a454611e17532b
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2025-01-19 16:48:43 +0530

    Project 18 | LayoutAndGeometry | challenge 2

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -17,11 +17,15 @@ struct ContentView: View {
                     GeometryReader { proxy in
                         Text("Row #\(index)")
                             .font(.title)
-                            .frame(maxWidth: .infinity)
+                            // challenge 2 - part 1
+                            .frame(width: 50 + (proxy.frame(in: .global).minY / fullView.size.height) * 355)
+                            // challenge 1
                             .background(colors[index % 7].opacity(
                                 max(0, min(1, (proxy.frame(in: .global).minY / fullView.size.height)))
                             ))
                             .rotation3DEffect(.degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                        // challenge 2 - part 2
+                            .frame(width: proxy.size.width, height: proxy.size.height)
                     }
                     .frame(height: 40)
                 }

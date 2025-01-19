34acec532b591765454c522413be40ed88818071
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2025-01-19 16:59:04 +0530

    Project 18 | LayoutAndGeometry | challenge 3

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -15,16 +15,28 @@ struct ContentView: View {
             ScrollView(.vertical) {
                 ForEach(0..<50) { index in
                     GeometryReader { proxy in
+                        let distanceFromTop = proxy.frame(in: .global).minY
+                        let totalHeight = fullView.size.height
+                        
                         Text("Row #\(index)")
                             .font(.title)
+                            .foregroundStyle(.white)
                             // challenge 2 - part 1
-                            .frame(width: 50 + (proxy.frame(in: .global).minY / fullView.size.height) * 355)
-                            // challenge 1
-                            .background(colors[index % 7].opacity(
-                                max(0, min(1, (proxy.frame(in: .global).minY / fullView.size.height)))
-                            ))
-                            .rotation3DEffect(.degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
-                        // challenge 2 - part 2
+                            .frame(width: 50 + (distanceFromTop / totalHeight) * 355)
+                            // challenge 3
+                            .background(
+                                Color(
+                                    hue: min(1, distanceFromTop / totalHeight),
+                                    saturation: 0.5,
+                                    brightness: 0.5
+                                )
+                                // challenge 1
+                                .opacity(
+                                    max(0, min(1, (distanceFromTop / totalHeight)))
+                                    )
+                            )
+                            .rotation3DEffect(.degrees(distanceFromTop - totalHeight / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                            // challenge 2 - part 2
                             .frame(width: proxy.size.width, height: proxy.size.height)
                     }
                     .frame(height: 40)

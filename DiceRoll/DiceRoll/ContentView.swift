ac11c212f0b9671013e4645675b9b4542886e3f7
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2025-01-20 14:04:50 +0530

    Milestone 6 | DiceRoll | initial commit

---
+++ DiceRoll/DiceRoll/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DiceRoll
+//
+//  Created by C3PO MBP on 20/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DiceRoll/DiceRoll/DiceRollApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollApp.swift
+//  DiceRoll
+//
+//  Created by C3PO MBP on 20/01/25.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

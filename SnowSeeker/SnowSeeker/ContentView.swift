760c350592642618c7bd3edeaa1df91ae83415cf
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2025-01-21 11:24:35 +0530

    Project 19 | SnowSeeker | NavigationSplitView - showing detail view by default on an iPhone

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationSplitView(columnVisibility: .constant(.all)) {
+        NavigationSplitView(preferredCompactColumn: .constant(.detail)) {
             NavigationLink("Primary") {
                 Text("New view")
             }

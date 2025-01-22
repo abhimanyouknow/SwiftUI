62066c224b812888d02223a784b2cc7c1f72bd70
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2025-01-22 10:29:09 +0530

    Project 19 | SnowSeeker | reset

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -7,32 +7,9 @@
 
 import SwiftUI
 
-@Observable
-class Player {
-    var name = "Anonymous"
-    var highScore = 0
-}
-
-struct HighScoreView: View {
-    @Environment(Player.self) var player
-    
-    var body: some View {
-        @Bindable var player = player
-            // this is needed if we want to use binding
-        
-        Stepper("Player's high score: \(player.highScore)", value: $player.highScore)
-    }
-}
-
 struct ContentView: View {
-    @State private var player = Player()
-    
     var body: some View {
-        VStack {
-            Text("Welcome!")
-            HighScoreView()
-        }
-        .environment(player)
+        Text("Hello, world!")
     }
 }
 

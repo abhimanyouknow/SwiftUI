63083b7c9402ecfe8e04cbca173b93bafc85a729
Abhimanyu Verma <abhimanyuverma73@gmail.com>
2025-01-25 23:25:26 +0530

    Project 19 | SnowSeeker | challenge 3

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -8,16 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    // challenge 3 - part 1
+    enum SortOption: String, CaseIterable, Identifiable {
+        case defaultOrder = "Default"
+        case name = "Alphabetically"
+        case country = "Country"
+        
+        var id: String { self.rawValue }
+    }
+    
     let resorts: [Resort] = Bundle.main.decode("resorts.json")
     
     @State private var favourites = Favourites()
     @State private var searchText = ""
+    // challenge 3 - part 2
+    @State private var sortOption: SortOption = .defaultOrder
     
     var filteredResorts: [Resort] {
+        // challenge 3 - part 3
+        let sortedResorts: [Resort]
+        switch sortOption {
+        case .defaultOrder:
+            sortedResorts = resorts
+        case .name:
+            sortedResorts = resorts.sorted(using: [SortDescriptor(\Resort.name)])
+        case .country:
+            sortedResorts = resorts.sorted(using: [SortDescriptor(\Resort.country), SortDescriptor(\Resort.name)])
+        }
+        
         if searchText.isEmpty {
-            resorts
+            return sortedResorts
         } else {
-            resorts.filter { $0.name.localizedStandardContains(searchText) }
+            return sortedResorts.filter { $0.name.localizedStandardContains(searchText) }
         }
     }
     
@@ -59,6 +81,20 @@ struct ContentView: View {
                 ResortView(resort: resort)
             }
             .searchable(text: $searchText, prompt: "Search for a resort")
+            // challenge 3 - part 4
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Menu {
+                        Picker("Sort by", selection: $sortOption) {
+                            ForEach(SortOption.allCases) { option in
+                                Text(option.rawValue).tag(option)
+                            }
+                        }
+                    } label: {
+                        Label("Sort", systemImage: "arrow.up.arrow.down")
+                    }
+                }
+            }
         } detail: {
             WelcomeView()
         }
